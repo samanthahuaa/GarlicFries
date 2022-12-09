a7bef2639ae84fc93a7e0a35ee4c2858c57b85f0
Vivian Teo <vteo30@stuy.edu>
2022-12-08 22:57:34 -0500

    more css/javascript for home.html:summary section, display property instead of opacity property, design choices

--- app/static/script.js
+++ app/static/script.js
@@ -8,44 +8,44 @@ function give_item() {
     if (document.getElementById('item').checked) {
         // display item by making opacity 1    
         var correct = document.getElementById('display-item')
-        correct.style.opacity = "1";
+        correct.style.display = "inline";
 
         // display the next step of the storyline, yes-no reaction
         var correct = document.getElementById('display-section-2')
-        correct.style.opacity = "1";
+        correct.style.display = "inline";
     }
     else {
         var correct = document.getElementById('display-item')
-        correct.style.opacity = "0";
+        correct.style.display = "none";
     }
   }
 
   function display_yes_no() {
     if (document.getElementById('yes-no').checked) {
         var correct = document.getElementById('display-yes-no')
-        correct.style.opacity = "1";
+        correct.style.display = "inline";
 
         // display the next step of the storyline, love-calc, show compatability
         var correct = document.getElementById('display-section-3')
-        correct.style.opacity = "1";
+        correct.style.display = "inline";
     }
     else {
         var correct = document.getElementById('display-yes-no')
-        correct.style.opacity = "0";
+        correct.style.display = "none";
     }
   }
 
   function display_love_calc() {
     if (document.getElementById('love-calc').checked) {
         var correct = document.getElementById('display-love-calc')
-        correct.style.opacity = "1";
+        correct.style.display = "inline";
 
         // display the next step of the storyline, date schedule
         var correct = document.getElementById('display-section-4')
-        correct.style.opacity = "1";        
+        correct.style.display = "inline";
     }
     else {
         var correct = document.getElementById('display-love-calc')
-        correct.style.opacity = "0";
+        correct.style.display = "none";
     }
   }
